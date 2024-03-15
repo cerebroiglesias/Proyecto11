e4e9238bf353e0362b8bdeadc26a2819a7604e58
pabloarieliglesias@gmail.com <pabloarieliglesias@gmail.com>
2024-03-15 14:13:46 -0300

    git init

---
+++ index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+let visitas = 0;
+app.set('views', './pages'); // specify the views directory
+app.set('view engine', 'ejs'); // register the template engine
+
+app.get('/datos', (req, res) => {
+    res.render('index', req.query);
+
+});
+
+const server = app.listen(PORT, () => {
+    console.log(`Server trabajando en http://localhost:${PORT}`);
+})
